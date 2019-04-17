af35cf53d37b893a59917012352c71d3b5dfd0fc
tinny <tinnystudios@gmail.com>
2019-04-17 13:51:59 +1000

    Add Notes

--- Beat-Showdown-Project/Assets/GameScene.cs
+++ Beat-Showdown-Project/Assets/GameScene.cs
@@ -5,13 +5,20 @@ public class GameScene : MonoBehaviour
 {
     public Stage Stage;
 
+    // This is where Flow would be useful?
     private IEnumerator Start()
     {
         yield return OpeningIntro();
         yield return Stage.Run();
+        yield return Ending();
     }
 
     private IEnumerator OpeningIntro()
+    {
+        yield break;
+    }
+
+    private IEnumerator Ending()
     {
         yield break;
     }
--- Beat-Showdown-Project/Assets/_Project/Scripts/Characters/Components/CharacterCombat.cs
+++ Beat-Showdown-Project/Assets/_Project/Scripts/Characters/Components/CharacterCombat.cs
@@ -4,6 +4,8 @@ namespace App.Characters.Components
 {
     public class CharacterCombat : MonoBehaviour
     {
+        // The attack depends on the Item you are holding
+
         public virtual void Attack()
         {
             Debug.Log("Attack!");
