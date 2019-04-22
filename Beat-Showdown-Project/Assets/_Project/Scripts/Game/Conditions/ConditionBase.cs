dc0905fd238d8464b079ee43c1652d7c71efafb3
Tin <tinnystudios@gmail.com>
2019-04-22 10:26:35 +1000

    Add ConditionBase

---
+++ Beat-Showdown-Project/Assets/_Project/Scripts/Game/Conditions/ConditionBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class ConditionBase<T> : ScriptableObject, IBind<T> where T : class
+{
+    public T Value;
+
+    public void Bind(T dependency)
+    {
+        Value = dependency;
+    }
+
+    public abstract bool Pass { get; }
+}
---
+++ Beat-Showdown-Project/Assets/_Project/Scripts/Game/Conditions/HpCondition.cs
@@ -0,0 +1,9 @@
+﻿using App.Characters.Models;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Condition/HpCondition")]
+public class HpCondition : ConditionBase<CharacterStatus>
+{
+    public int HpRequired;
+    public override bool Pass => Value.Hp > HpRequired;
+}
