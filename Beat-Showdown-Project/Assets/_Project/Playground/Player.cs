939f4b2a3dba9007224f61f89777f8fdb4f1f660
tinny <tinnystudios@gmail.com>
2019-04-23 13:21:18 +1000

    Add TestInput

---
+++ Beat-Showdown-Project/Assets/_Project/Playground/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Input;
+
+public class Player : MonoBehaviour
+{
+    public InputActionReference Attack;
+
+    private void Awake()
+    {
+        Attack.action.Enable();
+        Attack.action.performed += cont => Debug.Log("Attack");
+    }
+}
